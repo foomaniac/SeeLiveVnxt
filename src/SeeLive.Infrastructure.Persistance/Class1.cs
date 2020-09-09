76ae71dcb46d4e90e50611932fafa0c5d9ef3049
Dave Jones <foomaniac@gmail.com>
2020-09-09 22:31:55 +0100

    Initial commit - gradually building solution structure

--- src/SeeLive.Infrastructure.Persistance/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace SeeLive.Infrastructure.Persistance
-{
-    public class Class1
-    {
-    }
-}
---
+++ src/SeeLive.Infrastructure.Persistance/SeeLiveContext.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace SeeLive.Infrastructure.Persistance
+{
+    public class SeeLiveContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options.UseSql();
+    }
+}
