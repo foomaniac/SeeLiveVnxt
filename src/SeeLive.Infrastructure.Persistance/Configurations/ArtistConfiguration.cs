1297c498861bbfed11a1214fe3dd4445c9c08adf
Dave Jones <david.jones@nobiauk.com>
2020-10-25 11:35:45 +0000

    1. Building configuration 2. Added new entites to context

---
+++ src/SeeLive.Infrastructure.Persistance/Configurations/ArtistConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SeeLive.Core.Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SeeLive.Infrastructure.Persistance.Configurations
+{
+    public class ArtistConfiguration : IEntityTypeConfiguration<Artist>
+    {
+        public void Configure(EntityTypeBuilder<Artist> builder)
+        {
+            builder.HasKey(artist => artist.Id);
+
+            builder.Property(artist => artist.Name)
+                .IsRequired();
+        }
+    }
+}
