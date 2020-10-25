84bef55bd67da61fcca6b86be9adf68a031397d5
Dave Jones <david.jones@nobiauk.com>
2020-10-25 11:25:24 +0000

    Creating initial data model

--- src/SeeLive.Core.Domain/IEntity.cs
+++ src/SeeLive.Core.Domain/IEntity.cs
@@ -1,20 +1,20 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
 
 namespace SeeLive.Core.Domain
 {
-    public class Artist : IEntity<int>
-    {
-        public int Id { get; set; }
-        public string Name {get; set;}
+  public interface IEntity<TKey>
+    { 
+        public TKey Id { get; set; }
 
-        public string Bio {get; set;}
-
-        public string City {get; set;}
-
-        public string WebAddress {get; set;}
         public DateTime DateCreated { get; set; }
+
         public DateTime? DateModified { get; set; }
+
         public DateTime? DateDeleted { get; set; }
+
         public string CreatedByUser { get; set; }
     }
 }
