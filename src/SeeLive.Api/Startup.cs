cffc1f78f9b8e37e25c876c5200c0af8787c6493
Dave Jones <foomaniac@gmail.com>
2020-12-08 21:40:18 +0000

    Added missing files

--- src/SeeLive.Api/Startup.cs
+++ src/SeeLive.Api/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.IdentityModel.Tokens;
+using SeeLive.Infrastructure.Persistance.Configurations;
 
 namespace SeeLive.Application.Api
 {
