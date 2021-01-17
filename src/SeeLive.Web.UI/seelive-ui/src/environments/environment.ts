3cb8e74a0b49f9ccf8a7213edb634a81ac44b8e4
Dave Jones <foomaniac@gmail.com>
2021-01-17 21:20:46 +0000

    finished setting up auth with angular

--- src/SeeLive.Web.UI/seelive-ui/src/environments/environment.ts
+++ src/SeeLive.Web.UI/seelive-ui/src/environments/environment.ts
@@ -4,12 +4,12 @@
 
 export const environment = {
   production: false,
-  authority: 'https://localhost:5000',
+  authority: 'http://localhost:5000',
   clientId: 'seelive-ui',
   redirectUri: 'http://localhost:4200',
   responseType: 'id_token token',
   scope: 'openid SeeLive.Api',
-  seeLiveApi: 'https://localhost:6000/events',
+  seeLiveApi: 'http://localhost:6000/events',
 };
 
 /*
