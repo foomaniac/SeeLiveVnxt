2fd95bc45a071d3ab919b8f29da111d0528b61be
Dave Jones <david.jones@nobiauk.com>
2021-01-17 15:04:26 +0000

    Continuing auth config in angular app

--- src/SeeLive.Web.UI/seelive-ui/src/app/app.module.ts
+++ src/SeeLive.Web.UI/seelive-ui/src/app/app.module.ts
@@ -1,17 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, APP_INITIALIZER } from '@angular/core';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavigationComponent } from './navigation/navigation.component';
 
+import { AppService } from './services/app-service';
+import { AuthInterceptor } from './auth.interceptor';
+
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 
+export function initApp(appService: AppService) {
+  return () => appService.initApp();
+ }
+   
 @NgModule({
   declarations: [AppComponent, NavigationComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, MatToolbarModule, MatButtonModule],
-  providers: [],
+  imports: [BrowserModule, HttpClientModule, BrowserAnimationsModule, MatToolbarModule, MatButtonModule],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
+    { provide: APP_INITIALIZER, useFactory: initApp, deps: [AppService], multi: true }
+ ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
